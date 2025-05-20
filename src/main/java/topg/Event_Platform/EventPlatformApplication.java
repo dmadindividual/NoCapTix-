c8791c5855460546ae573f9714b2c8c141fa763e
dmadindividual <ayomidefalade@gmail.com>
2025-05-20 02:21:18 +0100

    Add dockerfile

--- src/main/java/topg/Event_Platform/EventPlatformApplication.java
+++ src/main/java/topg/Event_Platform/EventPlatformApplication.java
@@ -1,5 +1,6 @@
 package topg.Event_Platform;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -14,6 +15,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class EventPlatformApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(entry -> {
+			if (System.getenv(entry.getKey()) == null) {
+				System.setProperty(entry.getKey(), entry.getValue());
+			}
+		});
 		SpringApplication.run(EventPlatformApplication.class, args);
 	}
 
